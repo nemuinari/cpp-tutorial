ae1c60f3950d00b906cbf7363d1cfcd6991dda66
raspy <lyukey0902@gmail.com>
2025-12-07 15:27:25 +0900

    practice: 二分探索

--- tutorial/main.c
+++ tutorial/main.c
@@ -5,13 +5,25 @@ int data[] = {1, 7, 11, 12, 33, 57, 63, 72, 89, 99};
 
 int main(void) {
     int target = 57;
-    for (int i = 0; i < DATA_NUM; i++) {
-        if (data[i] == target) {
-            if (data[i] == target) {
-                printf("data[%d] に見つかりました\n", i);
-                return 0;
-            }
+    int left_index = 0;
+    int right_index = DATA_NUM - 1;  // 9
+    int middle_index = 0;
+
+    while (left_index <= right_index) {
+        middle_index = (left_index + right_index) / 2;
+
+        printf("left:%d, middle:%d, right:%d\n", left_index, middle_index,
+               right_index);
+
+        if (data[middle_index] == target) {
+            printf("値 %d は data[%d] に見つかりました\n", target,
+                   middle_index);
+            return 0;
+        } else if (data[middle_index] < target) {
+            left_index = middle_index + 1;
+        } else {
+            right_index = middle_index - 1;
         }
     }
-    printf("見つかりませんでした\n");
+    printf("値 %d は見つかりませんでした\n", target);
 }
