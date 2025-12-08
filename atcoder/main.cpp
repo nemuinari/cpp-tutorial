bfcea3370eaff56b78e29da6f1d59b32097acc47
raspy <lyukey0902@gmail.com>
2025-12-08 10:35:04 +0900

    practice: atcoder

---
+++ atcoder/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cout << "こんにちは" << endl << "AtCoder" << endl;
+    return 0;
+}
+
+// https://atcoder.jp/contests/apg4b/tasks
+// B
