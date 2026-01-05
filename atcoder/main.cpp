76c3d33f084930c5963c835b209e23184a62d161
raspy <lyukey0902@gmail.com>
2026-01-05 16:34:05 +0900

    practice: atcoder

--- atcoder/main.cpp
+++ atcoder/main.cpp
@@ -3,36 +3,41 @@ using namespace std;
 
 int main()
 {
-    int N, S;
-    cin >> N >> S;
+    int N, M;
+    cin >> N >> M;
 
-    vector<int> A(N), P(N);
-
-    for (int i = 0; i < N; i++)
+    vector<int> A(M), B(M);
+    for (int i = 0; i < M; i++)
     {
-        cin >> A.at(i);
-    }
-    for (int i = 0; i < N; i++)
-    {
-        cin >> P.at(i);
+        cin >> A.at(i) >> B.at(i);
     }
 
-    // リンゴ・パイナップルをそれぞれ1つずつ購入するとき合計S円になるような買い方が何通りあるか
-    // ここにプログラムを追記
-    int count = 0;
+    // 1. 2次元配列の宣言（N x N を '-' で初期化）
+    vector<vector<char>> results(N, vector<char>(N, '-'));
 
-    for (int x : A)
+    // 2. 試合結果を書き込む
+    for (int i = 0; i < M; i++)
     {
-        for (int y : P)
+        // インデックスに合わせるため -1 する
+        int winner = A.at(i) - 1;
+        int loser = B.at(i) - 1;
+
+        results.at(winner).at(loser) = 'o'; // 勝者に 'o'
+        results.at(loser).at(winner) = 'x'; // 敗者に 'x'
+    }
+
+    // 3. 表の出力
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
         {
-            if (x + y == S)
+            cout << results.at(i).at(j);
+            if (j != N - 1)
             {
-                count++;
+                cout << " ";
             }
         }
+        cout << endl;
     }
-    cout << count << endl;
-
-    return 0;
 }
-// S - EX17
+// T - EX18
