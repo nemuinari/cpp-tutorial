34822d9a29cfe1b88890e89166d7903a82e833b1
raspy <lyukey0902@gmail.com>
2026-01-06 15:30:13 +0900

    feat: shell script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# $ ./run.sh 実行
+
+# C コンパイルと実行
+# gcc -std=c23 -Wall -Wextra -O2 main.c -o main && ./main
+
+# C++ コンパイルと実行
+g++ -std=c++23 -Wall -Wextra -O2 main.cpp -o main && ./main 
---
+++ study/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# C コンパイルと実行
+# gcc -std=c23 -Wall -Wextra -O2 main.c -o main && ./main
+
+# C++ コンパイルと実行
+g++ -std=c++23 -Wall -Wextra -O2 main.cpp -o main && ./main 
